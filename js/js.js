03b0a700782a0f298fbe743409715c54ceced850
Shubh Gawhade <51043720+shubhgawhade@users.noreply.github.com>
2022-02-25 23:48:52 +0530

    Shubh
    - fixed top navbar

--- js/js.js
+++ js/js.js
@@ -4,7 +4,7 @@ var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 function myFunction() {
-    if (window.pageYOffset >= sticky) {
+    if (window.scrollY >= sticky) {
         navbar.classList.add("sticky")
     } else {
         navbar.classList.remove("sticky");
