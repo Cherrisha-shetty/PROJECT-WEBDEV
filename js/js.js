78d3a6eb50132b4f204451110f28e7a7fea1dd28
Cherrisha-shetty <cherrisha.cs20@bmsce.ac.in>
2022-02-25 22:28:18 +0530

    nav

---
+++ js/js.js
@@ -0,0 +1,23 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+
+//hamburger
+function Function() {
+    var x = document.getElementById("navbar");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
