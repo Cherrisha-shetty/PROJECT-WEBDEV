7cad6619e48a552d8c8b2cbf217c856c63233cbd
Cherrisha-shetty <cherrisha.cs20@bmsce.ac.in>
2022-02-27 16:52:04 +0530

    cherry
    cart final, js fixed

--- Cart/JS/cart.js
+++ Cart/JS/cart.js
@@ -12,3 +12,34 @@ function decreaseValue() {
     value--;
     document.getElementById('number').value = value;
 }
+
+function increaseValuesecond() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('numbersecond').value = value;
+}
+
+function decreaseValuesecond() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('numbersecond').value = value;
+}
+
+
+function increaseValuethird() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('numberthird').value = value;
+}
+
+function decreaseValuethird() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('numberthird').value = value;
+}
